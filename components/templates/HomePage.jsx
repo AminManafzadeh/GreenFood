bc9e419b5698b1ce5c742d8372733f2d5995bd27
AminManafzadeh <amin.mah1375@gmail.com>
2025-07-20 13:00:17 -0700

    fixed a bog in homepage

--- components/templates/HomePage.jsx
+++ components/templates/HomePage.jsx
@@ -3,7 +3,7 @@ import Banner from "../modules/Banner";
 import Companies from "../modules/Companies";
 import Definition from "../modules/Definition";
 import Guide from "../modules/Guide";
-import Instruction from "../modules/instruction";
+import Instruction from "../modules/Instruction";
 import Restriction from "../modules/Restriction";
 
 function HomePage() {
